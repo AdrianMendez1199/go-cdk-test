f8002905a343cc3f53ef0ea507c357e89b1270bf
Adrian Mendez <adrian@blindcreator.com>
2024-09-24 20:10:43 -0400

    working on codepipeline config

--- go-cdk-test.go
+++ go-cdk-test.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/aws-cdk-go/awscdk/v2/pipelines"
 	"github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -16,23 +15,17 @@ type GoCdkTestStackProps struct {
 }
 
 func NewGoCdkTestStack(scope constructs.Construct, id string, props *GoCdkTestStackProps) awscdk.Stack {
-	var sprops awscdk.StackProps
-	if props != nil {
-		sprops = props.StackProps
-	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
-
-	// The code that defines your stack goes here
+	stack := awscdk.NewStack(scope, &id, &props.StackProps)
 
 	lambdaFunction := awscdklambdagoalpha.NewGoFunction(stack, jsii.String("go-cdk-test"), &awscdklambdagoalpha.GoFunctionProps{
-		Entry:   jsii.String("./cmd/api"),
-		Runtime: awslambda.Runtime_PROVIDED_AL2(),
+		FunctionName: jsii.String("get-user-lambda"),
+		Entry:        jsii.String("./cmd/api"),
+		Runtime:      awslambda.Runtime_PROVIDED_AL2(),
 		Environment: &map[string]*string{
 			"PRODUCTION": jsii.String("true"),
 		},
 	})
 
-	// Define the API Gateway REST API
 	api := awsapigateway.NewRestApi(stack, jsii.String("ApiGateway"), &awsapigateway.RestApiProps{
 		RestApiName: jsii.String("GoCdkTestApi"),
 		DeployOptions: &awsapigateway.StageOptions{
@@ -47,43 +40,66 @@ func NewGoCdkTestStack(scope constructs.Construct, id string, props *GoCdkTestSt
 	return stack
 }
 
+// Define a new stage that includes the GoCdkTestStack
+func NewGoCdkTestStage(scope constructs.Construct, id string, env *awscdk.Environment) awscdk.Stage {
+	stage := awscdk.NewStage(scope, &id, &awscdk.StageProps{
+		Env: env,
+	})
+
+	NewGoCdkTestStack(stage, "GoCdkTestStack", &GoCdkTestStackProps{
+		StackProps: awscdk.StackProps{
+			Env: env,
+		},
+	})
+
+	return stage
+}
+
+func NewPipelineStack(scope constructs.Construct, id string, env *awscdk.Environment) awscdk.Stack {
+	stack := awscdk.NewStack(scope, &id, &awscdk.StackProps{
+		Env: env,
+	})
+
+	githubSource := pipelines.CodePipelineSource_GitHub(jsii.String("AdrianMendez1199/go-cdk-test"), jsii.String("main"), &pipelines.GitHubSourceOptions{
+		Authentication: awscdk.SecretValue_SecretsManager(jsii.String("githubAccessToken"), nil),
+	})
+
+	pipeline := pipelines.NewCodePipeline(stack, jsii.String("Pipeline"), &pipelines.CodePipelineProps{
+		PipelineName: jsii.String("GoCdkTestPipeline"),
+		Synth: pipelines.NewShellStep(jsii.String("Synth"), &pipelines.ShellStepProps{
+			Input: githubSource,
+			Commands: &[]*string{
+				jsii.String("npm install -g aws-cdk"),
+				jsii.String("go mod tidy"),
+				jsii.String("cdk synth"),
+			},
+		}),
+	})
+
+	// Add the GoCdkTestStage to the pipeline
+	pipeline.AddStage(NewGoCdkTestStage(stack, "DeployStage", env), nil)
+
+	return stack
+}
+
 func main() {
 	defer jsii.Close()
 
 	app := awscdk.NewApp(nil)
 
-	NewGoCdkTestStack(app, "GoCdkTestStack", &GoCdkTestStackProps{
-		awscdk.StackProps{
-			Env: env(),
-		},
-	})
+	NewPipelineStack(app, "PipelineStack", env())
 
 	app.Synth(nil)
 }
 
 // env determines the AWS environment (account+region) in which our stack is to
-// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+// be deployed.
 func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
 	return nil
-
 	// Uncomment if you know exactly what account and region you want to deploy
 	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
 	// return &awscdk.Environment{
 	//  Account: jsii.String("123456789012"),
 	//  Region:  jsii.String("us-east-1"),
-	// }
-
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
 	// }
 }
